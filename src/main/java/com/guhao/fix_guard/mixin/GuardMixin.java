5fe6f4ea15f287df0dd7fcd286b1335705ad43dc
XcS <61822055+xssfww@users.noreply.github.com>
2024-10-05 00:45:56 +0800

    修复了遗留已久的bug
    data是不初始化的，启动是因为得不到data而崩溃的

--- src/main/java/com/guhao/fix_guard/mixin/GuardMixin.java
+++ src/main/java/com/guhao/fix_guard/mixin/GuardMixin.java
@@ -1,52 +1,48 @@
 package com.guhao.fix_guard.mixin;
 
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import yesman.epicfight.api.animation.types.StaticAnimation;
 import yesman.epicfight.skill.SkillContainer;
-import yesman.epicfight.skill.SkillDataKey;
-import yesman.epicfight.skill.SkillDataKeys;
+import yesman.epicfight.skill.SkillDataManager;
 import yesman.epicfight.skill.guard.GuardSkill;
 import yesman.epicfight.skill.guard.ParryingSkill;
-import yesman.epicfight.world.capabilities.entitypatch.player.PlayerPatch;
-import yesman.epicfight.world.capabilities.entitypatch.player.ServerPlayerPatch;
 import yesman.epicfight.world.capabilities.item.CapabilityItem;
 import yesman.epicfight.world.entity.eventlistener.PlayerEventListener;
-
-import javax.annotation.Nullable;
-
-
-@Mixin(value = ParryingSkill.class, priority = 5000, remap = false)
+@Mixin(value = {ParryingSkill.class}, remap = false)
 public abstract class GuardMixin extends GuardSkill {
-
+    @Final
     @Shadow
-    @Nullable
-    protected abstract StaticAnimation getGuardMotion(PlayerPatch<?> playerpatch, CapabilityItem itemCapability, BlockType blockType);
-
+    private static SkillDataManager.SkillDataKey<Integer> LAST_ACTIVE;//将未初始化的data复制过来
+    
+    @Final
+    @Shadow
+    public static SkillDataManager.SkillDataKey<Integer> PARRY_MOTION_COUNTER;//同上
+    
     public GuardMixin(Builder builder) {
         super(builder);
     }
-
+    
     @Inject(at = @At("HEAD"), method = "onInitiate", cancellable = true)
-    public void mixin_guard(SkillContainer container, CallbackInfo ci) {
+    public void InjectOnInitiate(SkillContainer container, CallbackInfo ci) {
         ci.cancel();
         super.onInitiate(container);
+        container.getDataManager().registerData(LAST_ACTIVE);//byd作者居然没初始化Data
+        container.getDataManager().registerData(PARRY_MOTION_COUNTER);//如果用上一代代码，下行的服务器物品使用监听器会因为得不到data而崩溃
         container.getExecuter().getEventListener().addEventListener(PlayerEventListener.EventType.SERVER_ITEM_USE_EVENT, EVENT_UUID, (event) -> {
-            CapabilityItem itemCapability = ((ServerPlayerPatch)event.getPlayerPatch()).getHoldingItemCapability(InteractionHand.MAIN_HAND);
+            CapabilityItem itemCapability = event.getPlayerPatch().getHoldingItemCapability(InteractionHand.MAIN_HAND);
             if (this.isHoldingWeaponAvailable(event.getPlayerPatch(), itemCapability, BlockType.GUARD) && this.isExecutableState(event.getPlayerPatch())) {
-                ((ServerPlayer)((ServerPlayerPatch)event.getPlayerPatch()).getOriginal()).startUsingItem(InteractionHand.MAIN_HAND);
+                event.getPlayerPatch().getOriginal().startUsingItem(InteractionHand.MAIN_HAND);
             }
-
-            int lastActive = (Integer)container.getDataManager().getDataValue((SkillDataKey) SkillDataKeys.LAST_ACTIVE.get());
-            if (((ServerPlayer)((ServerPlayerPatch)event.getPlayerPatch()).getOriginal()).tickCount - lastActive >= 0) {
-                container.getDataManager().setData((SkillDataKey)SkillDataKeys.LAST_ACTIVE.get(), ((ServerPlayer)((ServerPlayerPatch)event.getPlayerPatch()).getOriginal()).tickCount);
+            int lastActive = container.getDataManager().getDataValue(LAST_ACTIVE);
+            if (event.getPlayerPatch().getOriginal().tickCount - lastActive > 0) {
+                container.getDataManager().setData(LAST_ACTIVE, event.getPlayerPatch().getOriginal().tickCount);
             }
-
         });
     }
+    
 }
